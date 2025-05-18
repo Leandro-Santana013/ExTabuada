4746d17378bfce351db4a6fa8c43d7f4968f302b
Leandro-Santana013 <ls1393424@gmail.com>
2025-05-18 16:23:42 -0700

    logica

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int n;
+
+Console.Write("digite o numero da tabuada: ");
+n = int.Parse(Console.ReadLine()!);
+
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{n} x {i} = {n * i}");
+}
